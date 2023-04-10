3c35444098a1068cac9e02a59bdef3a61fc82db3
EngJao89 <eng_joao@outlook.com>
2023-04-10 15:56:05 -0300

    feat(ig46-history-card): implementa cartão de historico de exercícios

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,10 +1,16 @@
 import {Center, Text, VStack} from 'native-base';
+
 import { ScreenHeader } from '@components/ScreenHeader';
+import { HistoryCard } from '@components/HistoryCard';
 
 export function History(){
   return(
     <VStack flex={1}>
       <ScreenHeader title='Histórico' />
+
+      <HistoryCard />
+      <HistoryCard />
+      <HistoryCard />
     </VStack>
   )
 }
