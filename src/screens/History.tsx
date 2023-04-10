3748a94339f41f1b971d390f0a380380236809e8
EngJao89 <eng_joao@outlook.com>
2023-04-10 16:11:04 -0300

    feat(ig47-sectionlist): implementa lista de histórico de exercícios

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,16 +1,44 @@
-import {Center, Text, VStack} from 'native-base';
+import { useState } from 'react';
+import { Heading, VStack, SectionList, Text, useToast } from 'native-base';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { HistoryCard } from '@components/HistoryCard';
 
 export function History(){
+  const [exercises, setExercises] = useState([
+    {
+      title: "26.08.23",
+      data: ['Puxada Frontal', 'Remada Unilateral']
+    },
+    {
+      title: "27.08.23",
+      data: ["Puxada Fronta"]
+    },
+  ]);
+
   return(
     <VStack flex={1}>
       <ScreenHeader title='Histórico' />
 
-      <HistoryCard />
-      <HistoryCard />
-      <HistoryCard />
+      <SectionList 
+            sections={exercises}
+            keyExtractor={item => item}
+            renderItem={({ item }) => <HistoryCard /> }
+            renderSectionHeader={({ section }) => (
+              <Heading color="gray.200" fontSize="md" mt={10} mb={3} fontFamily="heading">
+                {section.title}
+              </Heading>
+            )}
+            px={8}
+            contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center' }}
+            ListEmptyComponent={() => (
+              <Text color="gray.100" textAlign="center">
+                Não há exercícios registrados ainda. {'\n'}
+                Vamos fazer exercícios hoje?
+              </Text>
+            )}
+            showsVerticalScrollIndicator={false}
+          />
     </VStack>
   )
 }
