c37241e870eef14bd0516d0cc79e47839237a290
EngJao89 <eng_joao@outlook.com>
2023-04-12 20:43:26 -0300

    feat(ig59-imagem-exercicio): implementa imagem em detalhes de exercícios

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -40,6 +40,18 @@ export function Exercise(){
           </HStack>
         </HStack>
       </VStack>
+
+      <VStack p={8}>
+        <Image
+          w="full"
+          h={80}
+          source={{ uri: `https://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg` }}
+          alt="Nome do exercício"
+          mb={3}
+          resizeMode="cover"
+          rounded="lg"
+        />
+      </VStack>
     </VStack>
   )
 }
