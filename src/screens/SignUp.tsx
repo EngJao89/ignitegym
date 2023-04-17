49f320009cdce687cb5d6fbe9375c898d79b3c18
EngJao89 <eng_joao@outlook.com>
2023-04-16 21:38:02 -0300

    feat(ig72-input-com-estados): implementa recebimento de dados via estados

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from "native-base";
 
@@ -8,10 +9,24 @@ import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
 export function SignUp() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+
   const navigation = useNavigation();
 
   function handleGoBack() {
     navigation.goBack();
+  }
+
+  function handleSignUp(){
+    console.log({
+      name,
+      email,
+      password,
+      passwordConfirm
+    })
   }
 
   return(
@@ -38,21 +53,33 @@ export function SignUp() {
             Crie sua conta
           </Heading>
 
-          <Input placeholder="Nome"/>
+          <Input 
+            placeholder="Nome"
+            onChangeText={setName}
+          />
 
           <Input 
             placeholder="E-mail" 
             keyboardType="email-address"
             autoCapitalize="none"
+            onChangeText={setEmail}
           />
 
           <Input 
             placeholder="Senha" 
             secureTextEntry
+            onChangeText={setPassword}
+          />
+
+          <Input 
+            placeholder="Confirmar Senha" 
+            secureTextEntry
+            onChangeText={setPasswordConfirm}
           />
 
           <Button 
             title="Criar e acessar" 
+            onPress={handleSignUp} 
           />
         </Center>
 
