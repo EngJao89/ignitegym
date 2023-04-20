9b3d3b47138435fe7840102197239b9032fc3979
EngJao89 <eng_joao@outlook.com>
2023-04-20 14:21:47 -0300

    refactor(ig88-melhoria-cadastro): melhora a criação de novos usuários com axios

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -5,6 +5,8 @@ import { useForm, Controller } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 
+import { api } from "@services/api";
+
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
 
@@ -37,17 +39,8 @@ export function SignUp() {
   }
 
   async function handleSignUp({ name, email, password, password_confirm }: FormDataProps){
-    const response = await fetch('http://192.168.18.5:3333/users', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({name, email, password})
-    });
-
-    const data = await response.json();
-    console.log(data);
+    const response = await api.post('/users', {name, email, password});
+    console.log(response.data);
   }
 
   return(
