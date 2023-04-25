fa5efe9d0a9c7736a96ea1ba4c8a79822792d730
EngJao89 <eng_joao@outlook.com>
2023-04-25 16:02:02 -0300

    refactor(ig95-unificando-provider-context): melhora provider de contexto com unificação

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,8 +1,27 @@
-import { createContext } from "react";
+import { createContext, ReactNode } from "react";
 import { UserDTO } from "@dtos/UserDTO";
 
 export type AuthContextDataProps = {
   user: UserDTO;
 }
 
+type AuthContextProviderProps = {
+  children: ReactNode;
+}
+
 export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
+
+export function AuthContextProvider({ children }: AuthContextProviderProps){
+  return(
+    <AuthContext.Provider value={{
+      user: {
+        id: '1',
+        name: 'João Ricardo',
+        email: 'joao.ribeiro@gmail.com',
+        avatar: 'joao.png'
+      }
+    }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
