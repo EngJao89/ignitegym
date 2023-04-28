bd0c4dc98f6172c5dad88c253ebdc0ef54ec2dd9
EngJao89 <eng_joao@outlook.com>
2023-04-27 23:46:47 -0300

    feat(ig109-exibindo-nome-usuario): implementa nome de usuário salvo no servidor

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -2,9 +2,14 @@ import { TouchableOpacity } from 'react-native';
 import { Heading, HStack, Text, VStack, Icon } from 'native-base';
 import { MaterialIcons } from '@expo/vector-icons';
 
+import { useAuth } from '@hooks/useAuth';
+
+import { api } from '@services/api';
+
 import { UserPhoto } from "@components/UserPhoto";
 
 export function HomeHeader() {
+  const { user } = useAuth();
 
   return (
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
@@ -21,7 +26,7 @@ export function HomeHeader() {
         </Text>
 
         <Heading color="gray.100" fontSize="md" fontFamily="heading">
-          João Ricardo
+          {user.name}
         </Heading>
       </VStack>
 
