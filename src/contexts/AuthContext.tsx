3046a581b25c033c9a86bbba6d66ef8c3eed63af
EngJao89 <eng_joao@outlook.com>
2023-05-01 21:08:20 -0300

    feat(ig115-anexando-token): implementa anexo de token ás requisições

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
 
+import { storageAuthTokenSave } from '@storage/storageAuthToken';
 import { storageUserGet, storageUserRemove, storageUserSave } from '@storage/storageUser';
 
 import { api } from '@services/api';
@@ -22,13 +23,25 @@ export function AuthContextProvider({ children }: AuthContextProviderProps){
   const [user, setUser] = useState<UserDTO>({} as UserDTO);
   const [isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true);
 
+  async function storageUserAndTokenSave(userData: UserDTO, token: string) {
+    try {
+      setIsLoadingUserStorageData(true);
+
+      await storageUserSave(userData);
+      await storageAuthTokenSave( token );      
+    } catch (error) {
+      throw error
+    } finally {
+      setIsLoadingUserStorageData(false);
+    }
+  }
+
   async function signIn(email: string, password: string) {
     try{
       const { data } = await api.post('/sessions', {email, password});
 
-      if(data.user && data.token){
-        setUser(data.user);
-        storageUserSave(data.user);
+      if(data.user && data.token) {
+        await storageUserAndTokenSave(data.user, data.token);
       }
     }catch (error){
       throw error;
