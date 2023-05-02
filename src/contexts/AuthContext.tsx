a31946f603996c913d6e2f20073a9f80d9b57ad2
EngJao89 <eng_joao@outlook.com>
2023-05-01 21:33:50 -0300

    feat(ig116-recuperando-token-do-storage): implementa função que recupera o token do usuário

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
 
-import { storageAuthTokenSave } from '@storage/storageAuthToken';
+import { storageAuthTokenSave, storageAuthTokenGet } from '@storage/storageAuthToken';
 import { storageUserGet, storageUserRemove, storageUserSave } from '@storage/storageUser';
 
 import { api } from '@services/api';
@@ -23,12 +23,18 @@ export function AuthContextProvider({ children }: AuthContextProviderProps){
   const [user, setUser] = useState<UserDTO>({} as UserDTO);
   const [isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true);
 
+  async function userAndTokenUpdate(userData: UserDTO, token: string) {
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+    setUser(userData);
+  }
+
   async function storageUserAndTokenSave(userData: UserDTO, token: string) {
     try {
       setIsLoadingUserStorageData(true);
 
       await storageUserSave(userData);
-      await storageAuthTokenSave( token );      
+      await storageAuthTokenSave( token );
     } catch (error) {
       throw error
     } finally {
@@ -38,10 +44,11 @@ export function AuthContextProvider({ children }: AuthContextProviderProps){
 
   async function signIn(email: string, password: string) {
     try{
-      const { data } = await api.post('/sessions', {email, password});
+      const { data } = await api.post('/sessions', { email, password });
 
-      if(data.user && data.token) {
+      if(data.user && data.token && data.refresh_token) {
         await storageUserAndTokenSave(data.user, data.token);
+        userAndTokenUpdate(data.user, data.token)
       }
     }catch (error){
       throw error;
@@ -65,9 +72,10 @@ export function AuthContextProvider({ children }: AuthContextProviderProps){
       setIsLoadingUserStorageData(true);
 
       const userLogged = await storageUserGet();
+      const token  = await storageAuthTokenGet();
       
-      if(userLogged) {
-        setUser(userLogged);
+      if(token && userLogged) {
+        userAndTokenUpdate(userLogged, token);
       } 
     } catch (error) {
       throw error
