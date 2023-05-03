f4b8fa1031f2826b8652bb771eeb627b50194e37
EngJao89 <eng_joao@outlook.com>
2023-05-03 09:53:01 -0300

    feat(ig123-loading-home): implementa componente de loading em home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -11,8 +11,11 @@ import { HomeHeader } from '@components/HomeHeader';
 import { ExerciseCard } from '@components/ExerciseCard';
 
 import { AppNavigatorRoutesProps } from '@routes/app.routes';
+import { Loading } from '@components/Loading';
 
 export function Home(){
+  const [isLoading, setIsLoading] = useState(true);
+
   const [groups, setGroups] = useState<string[]>([]);
   const [exercises, setExercises] = useState<ExerciseDTO[]>([]);
 
@@ -44,6 +47,7 @@ export function Home(){
 
   async function fecthExercisesByGroup() {
     try {
+      setIsLoading(true);
       const response = await api.get(`/exercises/bygroup/${groupSelected}`);
 
       setExercises(response.data);
@@ -57,6 +61,8 @@ export function Home(){
         placement: 'top',
         bgColor: 'red.500'
       })
+    } finally {
+      setIsLoading(false);
     }
   }
 
@@ -94,32 +100,36 @@ export function Home(){
         minH={10}
       />
 
-      <VStack>
-        <HStack justifyContent="space-between" mb={5}>
-          <Heading color="gray.200" fontSize="md" fontFamily="heading">
-            Exercícios
-          </Heading>
+      {
+        isLoading ? <Loading /> :
+        <VStack px={8}>
+          <HStack justifyContent="space-between" mb={5}>
+            <Heading color="gray.200" fontSize="md" fontFamily="heading">
+              Exercícios
+            </Heading>
 
-          <Text color="gray.200" fontSize="sm">
-            {exercises.length}
-          </Text>
-        </HStack>
+            <Text color="gray.200" fontSize="sm">
+              {exercises.length}
+            </Text>
+          </HStack>
 
-        <FlatList 
-          data={exercises}
-          keyExtractor={item => item.id}
-          renderItem={({ item }) => (
-            <ExerciseCard 
-              onPress={() => handleOpenExerciseDetails()}
-              data={item}
-            />
-          )}
-          showsVerticalScrollIndicator={false}
-          _contentContainerStyle={{
-            paddingBottom: 20
-          }}
-        />
-      </VStack>
+          <FlatList 
+            data={exercises}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => (
+              <ExerciseCard 
+                onPress={() => handleOpenExerciseDetails()}
+                data={item}
+              />
+            )}
+            showsVerticalScrollIndicator={false}
+            _contentContainerStyle={{
+              paddingBottom: 20
+            }}
+          />
+
+        </VStack>
+      }
     </VStack>
   )
 }
